7b7d7f91ebd414081ffb9529526dc76824429f13
Shingo Sato <shinsugar@gmail.com>
2019-07-27 22:59:37 +0900

    Fix Homebrew setup + update vscode setup

--- setup/darwin/homebrew.sh
+++ setup/darwin/homebrew.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
+BREWFILES="${DOTFILES}/brewfiles"
 
 [[ $(command -v brew) ]] || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
 brew tap Homebrew/bundle
-brew bundle --file="${DOTFILES}/Brewfiles/default"
-brew bundle --file="${DOTFILES}/Brewfiles/fonts"
-brew bundle --file="${DOTFILES}/Brewfiles/casks"
-brew bundle --file="${DOTFILES}/Brewfiles/mas"
+brew bundle --file="${BREWFILES}/default"
+brew bundle --file="${BREWFILES}/fonts"
+brew bundle --file="${BREWFILES}/casks"
+brew bundle --file="${BREWFILES}/mas"
---
+++ setup/darwin/vscode.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eux
+
+DOTFILES=$(cd $(dirname $0)/../.. && pwd)
+VSCODE_FILES="${DOTFILES}/vscode"
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+if [ -d '/Applications/Visual Studio Code.app' ]; then
+  cat "${VSCODE_FILES}/Extensions" | while read -r EXT; do
+    code --install-extension $EXT
+  done
+
+  code --list-extensions > "${VSCODE_FILES}/Extensions"
+
+  if [ -d "${HOME}/Library" ]; then
+    mkdir -p "${HOME}/Library/Application Support"
+    rm -rf "${HOME}/Library/Application Support/Code/User"
+    ln -s "${VSCODE_FILES}/vscode/User" "${HOME}/Library/Application Support/Code/User"
+  fi
+else
+  echo 'Visual Studio Code has not been installed.'
+fi
