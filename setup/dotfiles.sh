bdd78336008c264cbed869591de8787ea2628492
Shingo Sato <shinsugar@gmail.com>
2019-07-28 00:04:12 +0900

    Fix setup/dotfiles.sh

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,8 +1,32 @@
 set -eux
 
 DOTFILES=$(cd $(dirname $0)/.. && pwd)
+BACKUP_DIR="${HOME}/.dotfiles.backups"
 
-for d in $DOTFILES/.* ; do
-  /bin/sh $d
-  ln -s $d $(basename $d)
+backup() {
+  if [[ -e $1 ]]; then
+    BASENAME=$(basename $1)
+    BACKUP_COUNT=$(find $BACKUP_DIR/$BASENAME* -maxdepth 0 2> /dev/null | wc -l | sed 's/ //g')
+
+    if [[ $dotfile_count -ne '0' ]]; then
+      mv $1 $BACKUPDIR/$BASENAME.$BACKUP_COUNT
+    else
+      mv $1 $BACKUPDIR/$BASENAME
+    fi
+  fi
+}
+
+symlink() {
+  ORG=$1
+  DST=$2
+  echo "Symlinking: ${ORG} -> ${DST}"
+  ln -s $ORG $DST
+}
+
+for f in $(find ${DOTFILES} -maxdepth 1 -type f -name .\*) ; do
+  BASENAME=$(basename $f)
+  echo $BASENAME
+  echo $f
+  backup "${HOME}/.${BASENAME}"
+  symlink $f "${HOME}/.${BASENAME}"
 done
