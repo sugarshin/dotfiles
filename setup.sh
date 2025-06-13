dd16dc5c53dfdc5b1be7e37556f0f9f2a72bf333
Shingo Sato <shinsugar@gmail.com>
2025-06-13 15:47:20 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ setup() {
 
 setup "$DOTFILES"/setup/*.sh
 
-# if [ "$(uname -s)" = 'Darwin' ]; then
-#   setup "$DOTFILES/setup/darwin/*.sh"
-#   setup "$DOTFILES/setup/darwin/post/*.sh"
-# fi
+if [ "$(uname -s)" = 'Darwin' ]; then
+  setup "$DOTFILES/setup/darwin/defaults.sh"
+  # setup "$DOTFILES/setup/darwin/*.sh"
+  # setup "$DOTFILES/setup/darwin/post/*.sh"
+fi
