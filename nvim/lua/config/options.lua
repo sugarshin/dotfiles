439393c94c1467d680f8df692a4657fc5c191f0a
Shingo Sato <shinsugar@gmail.com>
2026-03-29 16:49:50 +0900

    Update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Add any additional options here
-vim.opt.spelllang:append("cjk")
+vim.opt.spell = false
 vim.opt.clipboard = "unnamedplus"
