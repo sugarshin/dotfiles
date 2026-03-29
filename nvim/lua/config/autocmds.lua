2329752ac3dbc5abc1c9923d3d005f31af924f41
Shingo Sato <shinsugar@gmail.com>
2026-03-29 16:53:08 +0900

    Update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,2 +1,5 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Add any additional autocmds here
+
+-- Disable LazyVim's wrap_spell autocmd for markdown etc.
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
