2e2c02281c7bf2eb83a072c953759a66f035ce53
Shingo Sato <shinsugar@gmail.com>
2026-04-02 20:15:04 +0900

    Update nvim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,5 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Add any additional autocmds here
 
--- Disable LazyVim's wrap_spell autocmd for markdown etc.
-vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- LazyVim sets wrap + spell for markdown etc.
+-- Keep wrap but disable spell only.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "norg", "rmd", "org" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
