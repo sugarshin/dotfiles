7e9fa27f4fed540e7f789e90d98ad120feacb1ce
Shingo Sato <shinsugar@gmail.com>
2026-04-21 19:44:37 +0900

    Disable markdown lint

---
+++ config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.markdown = {}
+    end,
+  },
+}
